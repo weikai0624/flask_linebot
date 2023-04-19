4716eed894422fdcf720a07a29d0e0cf50bf0e13
weikai <weikai@fucotech.com.tw>
2023-04-19 15:05:25 +0800

    Initial

---
+++ app.py
@@ -0,0 +1,38 @@
+from __future__ import unicode_literals
+import os
+from flask import Flask, request, abort
+from linebot import LineBotApi, WebhookHandler
+from linebot.exceptions import InvalidSignatureError
+
+from linebot.models import MessageEvent, TextMessage, TextSendMessage
+
+app = Flask(__name__)
+
+# LINE 聊天機器人的基本資料
+line_bot_api = LineBotApi(os.environ.get('access_token'))
+handler = WebhookHandler(os.environ.get('secret'))
+
+@app.route("/callback", methods=['POST'])
+def callback():
+    signature = request.headers['X-Line-Signature']
+
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+    return 'OK'
+
+@handler.add(MessageEvent, message=TextMessage)
+def echo(event):
+    
+    if isinstance(event.message, TextMessage):
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text=event.message.text)
+        )
+
+if __name__ == "__main__":
+    app.run()
